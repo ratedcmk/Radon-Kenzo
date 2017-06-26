c7b4637fd4fbc753bd7f155f2b3aaef5ee718347
umang96 <umangleekha3@gmail.com>
2017-06-26 15:02:46 +0530

    Update build tools

--- build/tools/radon.sh
+++ build/tools/radon.sh
@@ -18,15 +18,21 @@
     chmod 0660 /sys/module/lowmemorykiller/parameters/minfree
 
     if [ $MemTotal -gt 2000000 ]; then
-        echo 0 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
+        echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
         echo 10 > /sys/module/process_reclaim/parameters/pressure_min
         echo 1024 > /sys/module/process_reclaim/parameters/per_swap_size
         echo "18432,23040,27648,32256,55296,80640" > /sys/module/lowmemorykiller/parameters/minfree
         echo 81250 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
+	echo 536870912 /sys/block/zram0/disksize
     else
         echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
         echo 10 > /sys/module/process_reclaim/parameters/pressure_min
         echo 1024 > /sys/module/process_reclaim/parameters/per_swap_size
         echo "14746,18432,22118,25805,40000,55000" > /sys/module/lowmemorykiller/parameters/minfree
-        echo 81250 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min 
+        echo 81250 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
+	echo 268435456 /sys/block/zram0/disksize
     fi
+
+    #Enable zram
+    mkswap /dev/block/zram0
+    swapon /dev/block/zram0
